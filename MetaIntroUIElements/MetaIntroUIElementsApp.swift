ebaa1943cebe6b664d282b5a1b2dd844667c1524
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-03 12:31:58 -0600

    Initial Commit

---
+++ MetaIntroUIElements/MetaIntroUIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaIntroUIElementsApp.swift
+//  MetaIntroUIElements
+//
+//  Created by Mitch Andrade on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaIntroUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
